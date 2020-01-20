d617e44fac00a9b26c4960d1fa1342acd15ef934
Nauryzbayev Darmanbek <n.darman05@gmail.com>
2020-01-20 21:12:54 +0600

    Make the blog post SEO Friendly

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
+use App\Model\Post;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -30,9 +31,13 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-
         parent::boot();
+
+        $router = app('router');
+        $router->bind('post', function($slug){
+            return Post::published()->where('slug', $slug)->first();
+        });
+
     }
 
     /**
