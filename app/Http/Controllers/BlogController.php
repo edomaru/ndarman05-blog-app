f50a7bf8937a2c55ddec24455110a7739fd1dd44
Nauryzbayev Darmanbek <n.darman05@gmail.com>
2020-01-23 17:13:25 +0600

    Filter the posts by category - part 1

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,17 +4,34 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Model\Post;
+use App\Model\Category;
 
 class BlogController extends Controller
 {
     protected $limit = 3;
 
     public function index(){
+        $categories = Category::with(['posts' => function($query){
+            $query->published();
+        }])->orderBy('title', 'asc')->get();
         $posts = Post::with('author')
                             ->latestFirst()
                             ->published()
                             ->paginate($this->limit);
-        return view('blog.index', compact('posts'));
+        return view('blog.index', compact('posts', 'categories'));
+    }
+
+    public function category($id){
+        $categories = Category::with(['posts' => function($query){
+            $query->published();
+        }])->orderBy('title', 'asc')->get();
+
+        $posts = Post::with('author')
+                            ->latestFirst()
+                            ->published()
+                            ->where('category_id', $id)
+                            ->paginate($this->limit);
+        return view('blog.index', compact('posts', 'categories'));
     }
 
     public function show(Post $post){
