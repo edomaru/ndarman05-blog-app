740ec6d1a9157efd1e65b0b1f282c8cfb31410dc
Nauryzbayev Darmanbek <n.darman05@gmail.com>
2020-02-18 14:28:56 +0600

    Revert "Error: categories in sidebar.balde.php"
    This reverts commit c276c4a29b266844cf8fb5779a6106b419e504fa.

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -11,32 +11,30 @@ class BlogController extends Controller
     protected $limit = 3;
 
     public function index(){
-        
-
+        $categories = Category::with(['posts' => function($query){
+            $query->published();
+        }])->orderBy('title', 'asc')->get();
         $posts = Post::with('author')
                             ->latestFirst()
                             ->published()
                             ->paginate($this->limit);
-        return view('blog.index', compact('posts'));
+        return view('blog.index', compact('posts', 'categories'));
     }
 
-    public function category(Category $category){
-        $categoryName = $category->title;
-
-        $posts = $category->posts()
-                            ->with('author') 
-                            ->latestFirst()
-                            ->published()
-                            ->paginate($this->limit);
-                            
-        return view('blog.index', compact('posts', 'categoryName'));
-    }
-
-    public function show(Post $post){
+    public function category($id){
         $categories = Category::with(['posts' => function($query){
             $query->published();
         }])->orderBy('title', 'asc')->get();
 
-        return view('blog.show', compact('post', 'categories'));
+        $posts = Post::with('author')
+                            ->latestFirst()
+                            ->published()
+                            ->where('category_id', $id)
+                            ->paginate($this->limit);
+        return view('blog.index', compact('posts', 'categories'));
+    }
+
+    public function show(Post $post){
+        return view('blog.show', compact('post'));
     }
 }
--- app/Model/Category.php
+++ app/Model/Category.php
@@ -9,8 +9,4 @@ class Category extends Model
     public function posts(){
         return $this->hasMany(Post::class);
     }
-
-    public function getRouteKeyName(){
-        return 'slug';
-    }
 }
